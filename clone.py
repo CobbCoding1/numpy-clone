2ea6ac059ec2bba8c0897dc5edc07068db3b744a
CobbCoding <rircobb@gmail.com>
2023-08-21 20:27:12 -0400

    added some functions to reduce boilerplate code and handle errors

--- clone.py
+++ clone.py
@@ -6,8 +6,17 @@ class Array():
     def compare_type_and_size(self, other):
         if isinstance(other, Array) and len(self) == len(other):
             return(True)
+        elif not isinstance(other, Array):
+            return(TypeError)
         else:
-            return(False)
+            return(ValueError)
+
+    def handle_errors(self, error, operation):
+        if error == ValueError:
+            raise ValueError("Arrays must have the same length")
+        elif error == TypeError:
+            raise TypeError(f"An Array cannot be {operation} a non-Array type!")
+
 
     def __getItem__(self, index):
         return(self.data[index])
@@ -25,46 +34,43 @@ class Array():
         return str(self.data)
 
     def __add__(self, other):
-        if self.compare_type_and_size(other):
+        compared = self.compare_type_and_size(other)
+        if compared == True:
             new_data = [x + y for x, y in zip(self.data, other.data)]
             return Array(new_data)
-        elif len(self) != len(other):
-            raise ValueError("Arrays must have the same length")
         else:
-            raise TypeError("An Array cannot be added to a non-Array type!")
+            return(self.handle_errors(compared, "added to"))
 
     def __sub__(self, other):
-        if self.compare_type_and_size(other):
+        compared = self.compare_type_and_size(other)
+        if compared == True:
             new_data = [x - y for x, y in zip(self.data, other.data)]
             return Array(new_data)
-        elif len(self) != len(other):
-            raise ValueError("Arrays must have the same length")
         else:
-            raise TypeError("An Array cannot be added to a non-Array type!")
+            return(self.handle_errors(compared, "subtracted from"))
 
     def __mul__(self, other):
-        if self.compare_type_and_size(other):
+        compared = self.compare_type_and_size(other)
+        if compared == True:
             new_data = [x * y for x, y in zip(self.data, other.data)]
             return Array(new_data) 
-        elif len(self) != len(other):
-            raise ValueError("Arrays must have the same length!")
         else:
-            raise TypeError("An Array cannot be added to a non-Array type!")
+            return(self.handle_errors(compared, "multiplied by"))
 
     def __truediv__(self, other):
         if isinstance(other, (int, float)):
             new_data = [x / other for x in self.data]
             return Array(new_data)
-        elif self.compare_type_and_size(other):
+        
+        compared = self.compare_type_and_size(other)
+        if compared == True:
             new_data = [x / y for x, y in zip(self.data, other.data)]
             return Array(new_data)
-        elif len(self) != len(other):
-            raise ValueError("Arrays must have the same length!")
         else:
-            raise TypeError("An array cannot be added to a non-Array type!")
+            return(self.handle_errors(compared, "divided by"))
 
 arr1 = Array([1, 2, 3])
 arr2 = Array([2, 4, 6])
 
-print(arr1)
+print(arr1 / [1])
         
