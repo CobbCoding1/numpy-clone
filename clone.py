ffd492895ee68117acb8eb5a6ddff445e289aecf
CobbCoding <rircobb@gmail.com>
2023-08-22 13:35:55 -0400

    Added function to increase size of array

--- clone.py
+++ clone.py
@@ -17,11 +17,21 @@ class Array():
         elif error == TypeError:
             raise TypeError(f"An Array cannot be {operation} a non-Array type!")
 
+    def increase_size(self, new_size, fill_value=0):
+        if(new_size < self.size):
+            raise IndexError("Cannot reduce the size of the array through extension")
+        diff = new_size - len(self)
+        self.data.extend([fill_value] * diff)
+        self.size = new_size
 
     def __getitem__(self, index):
+        if(index > self.size):
+            raise IndexError("Array index out of range")
         return(self.data[index])
 
     def __setitem__(self, index, value):
+        if(index >= self.size):
+            raise IndexError("Array index out of range")
         self.data[index] = value
 
     def __len__(self):
@@ -73,6 +83,6 @@ arr1 = Array([1, 2, 3])
 arr2 = Array([2, 4, 6])
 
 print(arr1)
-arr1[0] = 27
-print(arr1[0])
-        
+arr1[2] = 27
+print(arr1[2])
+print(arr1) 
